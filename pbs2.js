c246690276ac92b3aa200cdfbdc0f22f1bf0f8dc
Tiel <165927582+CZKDYN@users.noreply.github.com>
2024-05-01 01:57:28 +0800

    pbs2.js

--- pbs2.js
+++ pbs2.js
@@ -1,53 +1,26 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyC66oIPdf9E3S7Acky-6bWtfOLpKxUHQZU",
-  authDomain: "srpp-92b75.firebaseapp.com",
-  databaseURL: "https://srpp-92b75-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "srpp-92b75",
-  storageBucket: "srpp-92b75.appspot.com",
-  messagingSenderId: "657851773818",
-  appId: "1:657851773818:web:80ea22f26aca184d631cc3",
-  measurementId: "G-K8Y6H38TB9"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-var bookingFormDB = firebase.database().ref("booking-Form");
-
-document.getElementById("booking-form").addEventListener("submit", submitForm);
-
-function submitForm(e) {
-  e.preventDefault();
-
-  var name = getElementVal("name");
-  var email = getElementVal("email");
-  var contact = getElementVal("contact");
-  var date = getElementVal("date");
-  var startTime = getElementVal("start-time");
-  var hours = getElementVal("hours");
-
-  saveBooking(name, email, contact, date, startTime, hours);
-
-  if (hours > 12) {
-    alert("Maximum booking duration is 12 hours.");
-    return;
-}
-
-}
-
-const saveBooking = (name, email, contact, date, startTime, hours) => {
-  var newBooking = bookingFormDB.push();
-
-  newBooking.set({
-    name: name,
-    email: email,
-    contact: contact,
-    date: date,
-    startTime: startTime,
-    hours: hours
-  });
-};
-
-const getElementVal = (id) => {
-  return document.getElementById(id).value;
-};
-
+document.addEventListener('DOMContentLoaded', function() {
+    const feedbackForm = document.getElementById('feedback-form');
+    
+    feedbackForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+    
+        const feedbackInput = document.getElementById('feedback');
+        const feedbackValue = feedbackInput.value.trim();
+        if (feedbackValue === '') {
+            alert('Please enter your feedback.');
+            feedbackInput.focus();
+            return;
+        }
+        showThankYouMessage();
+    });
+    
+    function showThankYouMessage() {
+        alert('Thank you for using this system and providing your feedback!');
+    }
+    
+    const bookParkingButton = document.getElementById('book-parking-btn');
+    
+    bookParkingButton.addEventListener('click', function() {
+        window.location.href = 'index.html';
+    });
+});
